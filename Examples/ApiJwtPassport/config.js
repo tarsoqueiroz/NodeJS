28375a1d0534898ba4cb172e1dba010d7fb3c058
Tarso Queiroz <tarsoqueiroz@gmail.com>
2016-10-25 12:17:22 -0200

    API protected by Passport

---
+++ Examples/ApiJwtPassport/config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by USER: tarso.
+ * On         DATE: 24/10/16.
+ * By         NAME: config.js.
+ */
+
+// config.js
+module.exports = {
+  jwtSecret: "jwtS3cr3t",
+  jwtSession: {session: false}
+};
---
+++ Examples/ApiJwtPassport/users.js
@@ -0,0 +1,17 @@
+/**
+ * Created by USER: tarso.
+ * On         DATE: 24/10/16.
+ * By         NAME: users.js.
+ */
+
+// users.js
+// Fake list of users to be used in the authentication
+var users = [
+  {id: 1, name: "Tarso", email: "tarso@mail.com", password: "tarso123"},
+  {id: 2, name: "Cati",  email: "cati@mail.com",  password: "cati123"},
+  {id: 3, name: "Anna",  email: "anna@mail.com",  password: "anna123"},
+  {id: 4, name: "Breno", email: "breno@mail.com", password: "breno123"},
+  {id: 5, name: "Lola",  email: "lola@mail.com",  password: "lola123"}
+];
+
+module.exports = users;
