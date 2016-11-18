a25348d1a673bf2c69241ed8034f871dd5ce8afa
Tarso Queiroz <tarsoqueiroz@gmail.com>
2016-11-18 16:53:05 -0200

    webpack test

---
+++ Misc/offlineApp/webpack/basic/app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by USER: tarso.
+ * On         DATE: 18/11/16.
+ * By         NAME: app.js.
+ */
+
+document.write('welcome to my app');
+console.log('app loaded');
---
+++ Misc/offlineApp/webpack/basic/webpack.config.js
@@ -0,0 +1,12 @@
+/**
+ * Created by USER: tarso.
+ * On         DATE: 18/11/16.
+ * By         NAME: webpack.config.js.
+ */
+
+module.exports = {
+  entry: "./app.js",
+  output: {
+    filename: "bundle.js"
+  }
+}
