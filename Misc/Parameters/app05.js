2facb94ed3e13029de44fa4663ca0bf836a528b1
Tarso Queiroz <tarsoqueiroz@gmail.com>
2016-11-30 13:59:27 -0200

    Test of parm place on process.argv matrix

---
+++ Misc/Parameters/app05.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+/**
+ * Created by USER: tarso.
+ * On         DATE: 30/11/16.
+ * By         NAME: app05.
+ */
+
+/**
+ * If a file is executable or if executed by Node (as shell), the first parm is always 2.
+ */
+
+console.log(process.argv);
