e2e97e77f1ad5c8228a73adafd699ebd089b34a7
Tarso Queiroz <tarsoqueiroz@gmail.com>
2017-01-06 10:22:50 -0200

    ES6 tests

---
+++ EcmaScript6/ES6Syntax/es6-03-map.js
@@ -0,0 +1,15 @@
+/**
+ * Created by USER: tarso.
+ * On         DATE: 06/01/17.
+ * By         NAME: es6-03-map.js.
+ *
+ * Source: https://babeljs.io/
+ */
+
+'use strict';
+
+var aBase = [1, 2, 3];
+var aMapped = aBase.map(n => n + 1);
+
+console.log('Array base   :', aBase);
+console.log('Array mapped :', aMapped);
