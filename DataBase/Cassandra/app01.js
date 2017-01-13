14b10e0e36e92f57fd9169f2dd6ab22aad91593f
Tarso Queiroz <tarsoqueiroz@gmail.com>
2017-01-13 10:32:39 -0200

    Cassandra tests

--- DataBase/Cassandra/app01.js
+++ DataBase/Cassandra/app01.js
@@ -13,16 +13,28 @@ const assert = require('assert');
 const cassandra = require('cassandra-driver');
 const client = new cassandra.Client({ contactPoints: ['172.17.0.7', '172.17.0.8'], keyspace: 'demo' });
 
-const query = 'SELECT id, name FROM names';
+const query = 'SELECT * FROM names';
 
-client.execute(query, function (err, result) {
+client.execute(query, { prepare: true }, function (err, result) {
   if (err) {
     throw err;
   }
 
+//  console.log(result.rows);
+  console.log('Raw result... (typeof: %s)', typeof(result));
+  console.log(result);
+  console.log('  Host response: %s', result.info.queriedHost);
+
+  console.log('Raw result.rows... (typeof: %s)', typeof(result.rows));
+  console.log(result.rows);
+
+  console.log('Pretty result.rows[n]... (typeof: %s)', typeof(result.rows[0]));
+  console.log('   qty: %d', result.rows.length);
   for (var nSxi = 0, len = result.rows.length; nSxi < len; nSxi++) {
-    console.log(result.rows[nSxi]);
+    console.log('{ "id": %d, "name": "%s" }', result.rows[nSxi].id, result.rows[nSxi].name);
   }
+
+  process.exit();
 });
 
 /*
