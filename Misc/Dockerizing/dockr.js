5edeb4fc32d90854790737180782b19d762d95ee
Tarso Queiroz <tarsoqueiroz@gmail.com>
2017-01-16 17:43:27 -0200

    Dockerizing a Node.js web app

--- Misc/Dockerizing/dockr.js
+++ Misc/Dockerizing/dockr.js
@@ -4,6 +4,7 @@
  * By         NAME: dockr.js.
  *
  * Source: https://nodejs.org/en/docs/guides/nodejs-docker-webapp/
+ *         http://staxmanade.com/2016/07/run-multiple-docker-environments--qa--beta--prod--from-the-same-docker-compose-file-/
  */
 
 'use strict';
@@ -19,7 +20,9 @@ const SERVER_NAME = process.env.HTTPD_SNAME || "Noname Server";
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send(('Hello world from ' + SERVER_NAME + ' at ' + Date() + '\n'));
+//  res.send(('Hello world from ' + SERVER_NAME + ' at ' + Date() + '\n'));
+  res.writeHead(200, { "Content-Type": "text/plain"});
+  res.end(('Hello World from server <' + SERVER_NAME + '> at [' + Date() + ']\n'));
 });
 
 app.listen(PORT);
