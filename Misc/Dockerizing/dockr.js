f33dab9c6ad3d269625273da35208fce2e7468f8
Tarso Queiroz <tarsoqueiroz@gmail.com>
2017-01-16 16:10:11 -0200

    Dockerizing a Node.js web app

---
+++ Misc/Dockerizing/dockr.js
@@ -0,0 +1,26 @@
+/**
+ * Created by USER: tarso.
+ * On         DATE: 16/01/17.
+ * By         NAME: dockr.js.
+ *
+ * Source: https://nodejs.org/en/docs/guides/nodejs-docker-webapp/
+ */
+
+'use strict';
+
+// Modules
+const express = require('express');
+
+// Constants
+const PORT = 8080;
+const SERVER_NAME = process.env.HTTPD_SNAME || "Noname Server";
+
+// App
+const app = express();
+
+app.get('/', function (req, res) {
+  res.send(('Hello world from ' + SERVER_NAME + ' at ' + Date() + '\n'));
+});
+
+app.listen(PORT);
+console.log('Running "%s" on http://localhost:%d started at %s', SERVER_NAME, PORT, Date());
