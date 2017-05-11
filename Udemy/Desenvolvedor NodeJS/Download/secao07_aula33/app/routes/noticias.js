84bca6ea3ca73de46cb255a0ae9ba1c1266cda80
Tarso Queiroz <tarsoqueiroz@gmail.com>
2017-05-11 11:24:01 -0300

    Udemy course of NodeJS

---
+++ Udemy/Desenvolvedor NodeJS/Download/secao07_aula33/app/routes/noticias.js
@@ -0,0 +1,13 @@
+module.exports = function(application) {
+	
+	application.get('/noticias', function(req, res){
+	
+		var connection = application.config.dbConnection();
+		var noticiasModel = application.app.models.noticiasModel;
+
+		noticiasModel.getNoticias(connection, function(error, result){
+			res.render("noticias/noticias", {noticias : result});
+		});	
+	});
+
+};
