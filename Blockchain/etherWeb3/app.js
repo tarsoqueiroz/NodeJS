91f45e31ef0ea92479ab5674092bce0a564b603c
Tarso Queiroz <tarsoqueiroz@gmail.com>
2017-08-30 17:01:00 -0300

    Blockchain tests and codes

---
+++ Blockchain/etherWeb3/app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by USER: tarso.
+ * On         DATE: 30/08/17.
+ * By         NAME: app.js.
+ */
+
+'use strict';
+
+var web3 = require('web3')
+
+var version = web3.version.api;
+console.log(version); // "0.2.0"
+
