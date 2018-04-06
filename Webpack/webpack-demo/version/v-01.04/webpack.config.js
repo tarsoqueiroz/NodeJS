1515f8c65ad12f472259c8dcccbbb4d10de1998c
Tarso Queiroz <tarsoqueiroz@gmail.com>
2018-04-06 16:41:27 -0300

    Tutorial from Webpack.org

--- Webpack/webpack-demo/version/v-01.04/webpack.config.js
+++ Webpack/webpack-demo/version/v-01.04/webpack.config.js
@@ -1,32 +1,21 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
   entry: {
-    app:    './src/index.js'
+    app:    './src/index.js',
+    print:  './src/print.js'
   },
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist',
-    hot: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+    contentBase: './dist'
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       title: 'Output Management'
-    }),
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin()
+    })
   ],
   output: {
     filename: '[name].bundle.js',
