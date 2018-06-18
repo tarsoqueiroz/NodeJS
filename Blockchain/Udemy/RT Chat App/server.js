40fba67c66e68b461e99f5b9964b1de83d0eb3ac
Tarso Queiroz <tarsoqueiroz@gmail.com>
2018-06-18 16:57:35 -0300

    Socket.io chat examples

--- Blockchain/Udemy/RT Chat App/server.js
+++ Blockchain/Udemy/RT Chat App/server.js
@@ -12,6 +12,15 @@ mongo.connect('mongodb://127.0.0.1/chat', function (err, db) {
     socket.on('input', function (data) {
       var name = data.name;
       var message = data.message;
+      var whitespacePattern = /^\s*$/;
+
+      if (whitespacePattern.test(name) || whitespacePattern.test(message)) {
+        console.log('Invalid');
+      } else {
+        col.insert({name: name, message: message}, function () {
+          console.log('Inserte');
+        });null
+      }
 
       col.insert({name: name, message: message}, function () {
         console.log('Inserted');
---
+++ Real Time/socketioChat/index.js
@@ -0,0 +1,32 @@
+/**
+ * index.js
+ *
+ * Created by tarso on 18/06/18
+ */
+
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/jquery-1.11.1.js', function(req, res){
+  res.sendFile(__dirname + '/jquery-1.11.1.js');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
