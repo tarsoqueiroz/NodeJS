48f50dd927185fc7a16d11cc3e0dd207150e7da0
Tarso Queiroz <tarsoqueiroz@gmail.com>
2018-07-09 16:55:32 -0300

    WebAuth v2 - JsonDB Server

--- Auth/WebAuth.v2/webAuth.Server/server.js
+++ Auth/WebAuth.v2/webAuth.Server/server.js
@@ -15,38 +15,36 @@ const FileStore = require('session-file-store')(session);
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
-
-const users = [
-  {id: '2f24vvg', email: 'test@test.com', password: 'password'}
-];
+const axios = require('axios');
 
 // configure passport.js to use the local strategy
 passport.use(new LocalStrategy(
   { usernameField: 'email' },
   (email, password, done) => {
-    console.log('Inside local strategy callback');
-    // here is where you make a call to the database
-    // to find the user based on their username or email address
-    // for now, we'll just pretend we found that it was users[0]
-    const user = users[0];
-    if(email === user.email && password === user.password) {
-      console.log('Local strategy returned true');
-      return done(null, user);
-    }
+    axios.get(`http://localhost:5000/users?email=${email}`)
+      .then(res => {
+        const user = res.data[0]
+        if (!user) {
+          return done(null, false, { message: 'Invalid credentials.\n' });
+        }
+        if (password != user.password) {
+          return done(null, false, { message: 'Invalid credentials.\n' });
+        }
+        return done(null, user);
+      })
+      .catch(error => done(error));
   }
 ));
 
 // tell passport how to serialize the user
 passport.serializeUser((user, done) => {
-  console.log('Inside serializeUser callback. User id is save to the session file store here');
   done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  console.log('Inside deserializeUser callback');
-  console.log(`The user id passport saved in the session file store is: ${id}`);
-  const user = users[0].id === id ? users[0] : false;
-  done(null, user);
+  axios.get(`http://localhost:5000/users/${id}`)
+    .then(res => done(null, res.data) )
+    .catch(error => done(error, false))
 });
 
 // create the server
@@ -57,8 +55,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({
   genid: (req) => {
-    console.log('Inside the session middleware');
-    console.log(req.sessionID);
     return uuid() // use UUIDs for session IDs
   },
   store: new FileStore(),
@@ -71,42 +67,34 @@ app.use(passport.session());
 
 // create the homepage route at '/'
 app.get('/', (req, res) => {
-  console.log('Inside the homepage callback function');
-  console.log(req.sessionID);
-  res.send(`Hit home page. Received the unique id: ${req.sessionID}\n`);
+  res.send(`You got home page! Received the unique id: ${req.sessionID}\n`);
   res.end();
 });
 
 // create the login get and post routes
 app.get('/login', (req, res) => {
   console.log('Inside GET /login callback function');
-  console.log(req.sessionID);
   res.send(`You got the login page!\n`);
   res.end();
 });
 
 app.post('/login', (req, res, next) => {
-  console.log('Inside POST /login callback');
   passport.authenticate('local', (err, user, info) => {
-    console.log('Inside passport.authenticate() callback');
-    console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`);
-    console.log(`req.user: ${JSON.stringify(req.user)}`);
+    if(info) {return res.send(info.message)}
+    if (err) { return next(err); }
+    if (!user) { return res.redirect('/login'); }
     req.login(user, (err) => {
-      console.log('Inside req.login() callback');
-      console.log(`req.session.passport: ${JSON.stringify(req.session.passport)}`);
-      console.log(`req.user: ${JSON.stringify(req.user)}`);
-      return res.send('You were authenticated & logged in!\n');
+      if (err) { return next(err); }
+      return res.redirect('/authrequired');
     })
   })(req, res, next);
 });
 
 app.get('/authrequired', (req, res) => {
-  console.log('Inside GET /authrequired callback');
-  console.log(`User authenticated? ${req.isAuthenticated()}`);
   if(req.isAuthenticated()) {
-    res.send('you hit the authentication endpoint\n');
+    res.send('you hit the authentication endpoint\n')
   } else {
-    res.redirect('/');
+    res.redirect('/')
   }
 });
 
