f1d8b615bf07f86e6babcd1327ca3cb483996705
Tarso Queiroz <tarsoqueiroz@gmail.com>
2018-07-09 15:30:19 -0300

    Test of Node.JS function

---
+++ Auth/WebAuth.v2/webAuth.Client/test.js
@@ -0,0 +1,13 @@
+/**
+ * test.js
+ *
+ * Created by tarso on 09/07/18
+ */
+
+const dateNow = Math.floor(Date.now());
+const dateExp = Math.floor(dateNow / 1000) + 3600;
+const dateIat = Math.floor(dateNow / 1000);
+
+console.log(dateNow);
+console.log(dateExp);
+console.log(dateIat);
