a8033f45d21ce684c0261630ea5943a2ac99a055
Tarso Queiroz <tarsoqueiroz@gmail.com>
2018-08-17 16:46:24 -0300

    Test of ES6 funcionality

---
+++ EcmaScript6/ES6Syntax/es6-04-attribution-config.js
@@ -0,0 +1,12 @@
+/**
+ * es6-04-attribution-config.js
+ *
+ * Created by tarso on 15/08/18
+ */
+
+const PARM1 = 'Parâmetro 01';
+const PARM2 = 2;
+const PARM3 = true;
+const PARM4 = 'Parâmetro 04';
+
+module.exports = { PARM1, PARM2, PARM3, PARM4 };
---
+++ EcmaScript6/ES6Syntax/es6-04-attribution.js
@@ -0,0 +1,11 @@
+/**
+ * es6-04-attribution.js
+ *
+ * Created by tarso on 15/08/18
+ */
+'use strict';
+
+const { PARM1, PARM4 } = require('./es6-04-attribution-config');
+
+console.log(PARM1);
+console.log(PARM4);
