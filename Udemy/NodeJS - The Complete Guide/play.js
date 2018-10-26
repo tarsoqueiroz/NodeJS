95450b2dba8f4c8310de9cf3403388fd19eddf4b
Tarso Queiroz <tarsoqueiroz@gmail.com>
2018-10-26 17:07:16 -0300

    Section 2

---
+++ Udemy/NodeJS - The Complete Guide/play.js
@@ -0,0 +1,15 @@
+/**
+ * play.js
+ *
+ * Created by tarso on 26/10/18
+ */
+
+var name = 'Max';
+var age = 29;
+var hasHobbies = true;
+
+function summarizeUser() {
+  return
+}
+
+console.log(name);
