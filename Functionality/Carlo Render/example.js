f728c2d3672e61e261a9490c8208e899625abf18
Tarso Queiroz <tarsoqueiroz@gmail.com>
2019-02-01 17:02:24 -0200

    Test of Google Carlo render

---
+++ Functionality/Carlo Render/example.js
@@ -0,0 +1,27 @@
+/**
+ * example.js
+ *
+ * Author: tarso
+ * Date  : 30/01/19
+ * Source:
+ */
+'use strict';
+
+const carlo = require('carlo');
+
+(async () => {
+  // Launch the browser
+  const app = await carlo.launch();
+
+  // Terminate Node.js process on app window closing.
+  app.on('exit', () => process.exit());
+
+  // Tell carlo where your web files are located
+  app.serveFolder(__dirname);
+
+  // Expose 'env' function in the web environment
+  await app.exposeFunction('env', _ => process.env);
+
+  // Navigate to the main page of your app
+  await app.load('example2.html');
+})();
