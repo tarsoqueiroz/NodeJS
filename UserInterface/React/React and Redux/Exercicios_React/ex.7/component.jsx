ddd48c5b9ee0566f36a0b8c22dda8e619b4fdf0a
Tarso Queiroz <tarsoqueiroz@gmail.com>
2019-02-14 17:04:49 -0200

    Raw React session OK

---
+++ UserInterface/React/React and Redux/Exercicios_React/ex.7/component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// const Primeiro = props => (
+export default props => (
+    <h1>Primeiro Componente!</h1>
+)
+
+export const Segundo = props => <h1>Segundo Componente!</h1>
+// export { Segundo }
+
+// export { Primeiro, Segundo }
