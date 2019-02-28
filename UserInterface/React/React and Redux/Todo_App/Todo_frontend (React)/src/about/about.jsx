8451921b8af7341f74d3315b817011cb67b0c713
Tarso Queiroz <tarsoqueiroz@gmail.com>
2019-02-28 14:29:10 -0300

    Todo App - React version

---
+++ UserInterface/React/React and Redux/Todo_App/Todo_frontend (React)/src/about/about.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+  <div>
+    <PageHeader name='Sobre' small='Nós'></PageHeader>
+
+    <h2>Nossa História</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+       Donec ac diam in mi tempor fermentum facilisis sed leo. Sed commodo porta sem et molestie.
+       Sed id lectus hendrerit, tristique urna sit amet, volutpat ipsum.
+    </p>
+    
+    <h2>Missão e Visão</h2>
+    <p> Cras ullamcorper justo in augue finibus, sed tempor enim elementum. 
+        Nullam pretium aliquam nisl, eget posuere massa commodo tempus. 
+        Nulla iaculis arcu ac turpis porttitor, nec faucibus dolor scelerisque. 
+    </p>
+    
+    <h2>Imprensa</h2>
+    <p>In nec quam convallis, imperdiet massa nec, sollicitudin magna. 
+       Nulla tincidunt consequat dui ac accumsan. Fusce eget iaculis ex. 
+       Nam dignissim mauris et ex cursus luctus. 
+    </p>
+    
+  </div>
+)
