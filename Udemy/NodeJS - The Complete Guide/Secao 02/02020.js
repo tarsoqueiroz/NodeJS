71faace4ebeaa24c8decf28491063f3841daa618
Tarso Queiroz <tarsoqueiroz@gmail.com>
2019-04-29 14:42:37 -0300

    Section 03

---
+++ Udemy/NodeJS - The Complete Guide/Secao 02/02020.js
@@ -0,0 +1,4 @@
+const name = 'Tarso';
+const age = 52;
+
+console.log(`My name is ${ name } and I'm ${ age } years old!!!`);
---
+++ Udemy/NodeJS - The Complete Guide/Secao 03/app.js
@@ -0,0 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  console.log(`URL    : ${req.url} \nMethod : ${req.method} \nHeaders: ${req.headers}`);
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My first page</title></head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
+  res.end();
+});
+
+server.listen(3000);
