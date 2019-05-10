5ac3547d3d9ddf6a33715d19ef2bec1a6dc7ffcc
Tarso Queiroz <tarsoqueiroz@gmail.com>
2019-05-10 16:49:04 -0300

    Session 03

--- Udemy/NodeJS - The Complete Guide/Secao 03/app.js
+++ Udemy/NodeJS - The Complete Guide/Secao 03/app.js
@@ -1,13 +1,9 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-  console.log(`URL    : ${req.url} \nMethod : ${req.method} \nHeaders: ${req.headers}`);
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<html>');
-  res.write('<head><title>My first page</title></head>');
-  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-  res.write('</html>');
-  res.end();
-});
+const routes = require('./routes');
+
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
