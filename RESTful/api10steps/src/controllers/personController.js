9c68bc6d150f6ebd57d89205ea102a6ee2eb3af6
Tarso Queiroz <tarsoqueiroz@gmail.com>
2019-11-07 12:26:26 -0300

    API server in 10 steps

---
+++ RESTful/api10steps/src/controllers/personController.js
@@ -0,0 +1,22 @@
+/*
+
+
+*/
+
+exports.get = (req, res, next) => {
+  res.status(200).send('Requisição GET recebida com sucesso!');
+};
+
+exports.post = (req, res, next) => {
+  res.status(201).send('Requisição POST recebida com sucesso!');
+};
+
+exports.put = (req, res, next) => {
+  let id = req.params.id;
+  res.status(201).send(`Requisição PUT recebida com sucesso! ${id}`);
+};
+
+exports.delete = (req, res, next) => {
+  let id = req.params.id;
+  res.status(200).send(`Requisição DELETE recebida com sucesso! ${id}`);
+};
