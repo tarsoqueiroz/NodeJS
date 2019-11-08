49d9e69aedf0824b9c948010c767ec2a257d0028
Tarso Queiroz <tarsoqueiroz@gmail.com>
2019-11-08 12:38:34 -0300

    Adjusts

--- RESTful/restService/routes/clients.js
+++ RESTful/restService/routes/clients.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET clients listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   db.
 });
+
+router.post('/', (req, res, next) => {
+  var 
+})
 
 module.exports = router;
