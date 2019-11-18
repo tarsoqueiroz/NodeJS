bff275af493962a4396ed6749b5fe2a97f6b4304
Tarso Queiroz <tarsoqueiroz@gmail.com>
2019-11-18 14:59:07 -0300

    Test with random-id for random id generation

--- Modules/randomID/index.js
+++ Modules/randomID/index.js
@@ -48,3 +48,11 @@ console.log(`len=${nLen}, char set="${strPattern}"`);
 for (nSx1 = 0; nSx1 < 5; nSx1++) {
   console.log(`id.${nSx1}: ${randomId(nLen, strPattern)}`);
 }
+
+// 5 random id with twice 16 char of length separate with dot for lowercase and number
+console.log("*** 5 random id with 10 char of length");
+nLen = 16;
+console.log(`len=${nLen}`);
+for (nSx1 = 0; nSx1 < 5; nSx1++) {
+  console.log(`id.${nSx1}: ${randomId(nLen, "a0")}.${randomId(nLen, "a0")}`);
+}
