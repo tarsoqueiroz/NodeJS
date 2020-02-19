68cbad276cf1651c55d919295121a0b3b57632c5
Tarso Queiroz <tarsoqueiroz@gmail.com>
2020-02-19 17:11:18 -0300

    Ending sec 02

---
+++ Udemy/SvelteJS - The Complete Guide/Recursos/Section_02/base-syntax-03-new-component/src/ContactCard.svelte
@@ -0,0 +1,18 @@
+<style>
+
+</style>
+
+<div>
+    <header>
+        <div>
+            <img src="" alt="">
+        </div>
+        <div>
+            <h1>User Name</h1>
+            <h2>Job Title</h2>
+        </div>
+    </header>
+    <div>
+        <p>A short description</p>
+    </div>
+</div>
--- Udemy/SvelteJS - The Complete Guide/project-01app/src/App.svelte
+++ Udemy/SvelteJS - The Complete Guide/project-01app/src/App.svelte
@@ -1,10 +1,17 @@
 <script>
-	let name = 'Max';
-	let age = 22;
+	import ContactCard from './ContactCard.svelte'
 
-	$: uppercaseName = name.toUpperCase();
+	let name = 'Max'
+	let title = ''
+	let image = ''
+	let description = ''
 
-	// $: console.log(name);
+	let newName = 'Maximilian'
+	let age = 22
+
+	$: uppercaseName = name.toUpperCase()
+
+	// $: console.log(name)
 	// $: console.log(age)
 	$: {
 		console.log('Function with 2 variables')
@@ -14,19 +21,22 @@
 
 	$: if (name === 'Maximilian') {
 		console.log('It runs!!!')
-		age = 31;
+		age = 31
 	}
 
 	function incrementAge() {
-		age += 1;
+		age += 1
 	}
 
 	function changeName() {
-		if (name === 'Maximilian') {
-			name = 'Max';
-		} else {
-			name = 'Maximilian';
-		}
+		let auxName = name
+		name = newName
+		newName = auxName
+	}
+
+	function nameInput(event) {
+		const enteredValue = event.target.value
+		newName = enteredValue
 	}
 </script>
 
@@ -36,6 +46,18 @@
 	}
 </style>
 
-<h1>Hello {uppercaseName}, my age is {age}!</h1>
-<button on:click="{incrementAge}">Change Age</button>
-<button on:click="{changeName}">Change Name</button>
+<h1>Hello dear {uppercaseName}, my age is {age}!</h1>
+<button on:click="{incrementAge}">Change Age of user</button>
+<!-- 
+<button on:click="{changeName}">Change Name to '{newName}'</button>
+<input type="text" value="{name}" on:input="{nameInput}"> 
+-->
+<input type="text" bind:value="{name}" />
+<input type="text" bind:value="{title}" />
+<input type="text" bind:value="{image}" />
+<textarea rows="3" bind:value="{description}"></textarea>
+
+<ContactCard userName={name} 
+						 jobTitle={title} 
+						 {description} 
+						 userImage={image} />
