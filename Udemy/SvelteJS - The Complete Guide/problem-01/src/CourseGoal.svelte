e181288067e61e393f9e4cffcc31c308f4329545
Tarso Queiroz <tarsoqueiroz@gmail.com>
2020-02-19 23:54:10 -0300

    Task 01

---
+++ Udemy/SvelteJS - The Complete Guide/problem-01/src/CourseGoal.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let goal
+
+  $: doHighLight = goal.includes('!')
+</script>
+
+<style>
+  .highlight {
+    color: red;
+  }
+</style>
+
+<h1 class:highlight={doHighLight} >{goal}</h1>
