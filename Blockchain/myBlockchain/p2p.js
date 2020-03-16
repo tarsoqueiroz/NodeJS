ac957247b373aa374817c697e604ef04f6dc3f3e
Tarso Queiroz <tarsoqueiroz@gmail.com>
2020-03-16 16:47:47 -0300

    Phase 2 of my blockchain

--- Blockchain/myBlockchain/p2p.js
+++ Blockchain/myBlockchain/p2p.js
@@ -3,10 +3,16 @@ const crypto   = require('crypto')
 const Swarm    = require('discovery-swarm')
 const defaults = require('dat-swarm-defaults')
 const getPort  = require('get-port')
+const chain    = require('./chain')
 
 const peers = {}
 let   connSeq = 0
 let   channel = 'myBlockchain'
+
+let   MessageType = {
+  REQUEST_BLOCK: 'requestBlock',
+  RECEIVE_NEXT_BLOCK: 'receiveNextBlock'
+}
 
 const myPeerId = crypto.randomBytes(32)
 console.log(`myPeerId: ${myPeerId.toString('hex')}`)
@@ -18,7 +24,7 @@ const config = defaults({
 const swarm = Swarm(config);
 
 (async () => {
-  const port = await getPort()
+  const port = await getPort({ port: getPort.makeRange(5000, 5100) });
 
   swarm.listen(port)
   console.log(`Listening on port ${port}`)
@@ -41,12 +47,36 @@ const swarm = Swarm(config);
 
     conn.on('data', data => {
       let message = JSON.parse(data)
+
       console.log('--- Received Message start ---')
-      console.log(`  from: ${peerId.toString('hex')}`)
-      console.log(`  to  : ${peerId.toString(message.to)}`)
-      console.log(`  my  : ${myPeerId.toString('hex')}`)
-      console.log(`  type: ${JSON.stringify(message.type)}`)
+      // console.log(`  data: ${data}`)
+      console.log(`  from: ${peerId.toString('hex').substring(0,6)}\n  to  : ${myPeerId.toString('hex').substring(0,6)}\n  type: ${JSON.stringify(message.type)}`)
       console.log('--- Received Message end ---')
+
+      switch (message.type) {
+        case MessageType.REQUEST_BLOCK:
+          let requestedIndex = (JSON.parse(JSON.stringify(message.data))).index;
+          let requestedBlock = chain.getBlock(requestedIndex);
+          console.log('-----------REQUEST_BLOCK-------------');
+          if (requestedBlock) {
+            writeMessageToPeerToId(peerId.toString('hex'), MessageType.RECEIVE_NEXT_BLOCK, requestedBlock);
+          }
+          else {
+            console.log(`No block found @ index: ${requestedIndex}`);
+          }
+          console.log('-----------REQUEST_BLOCK-------------');
+          break;
+
+        case MessageType.RECEIVE_NEXT_BLOCK:
+          let nextBlockIndex = chain.getLatestBlock().index+1;
+          console.log('-----------RECEIVE_NEXT_BLOCK-------------');
+          chain.addBlock(JSON.parse(JSON.stringify(message.data)));
+          console.log(JSON.stringify(chain.blockchain));
+          console.log(`-- request next block @ index: ${nextBlockIndex}`);
+          writeMessageToPeers(MessageType.REQUEST_BLOCK, {index: nextBlockIndex});
+          console.log('-----------RECEIVE_NEXT_BLOCK-------------');
+          break;
+      }
     })
 
     conn.on('close', () => {
@@ -65,15 +95,10 @@ const swarm = Swarm(config);
   })
 })()
 
-
-setTimeout(() => {
-  writeMessageToPeers('hello', null)
-}, 10000)
-
 writeMessageToPeers = (type, data) => {
   for (let id in peers) {
     console.log('--- writeMessageToPeers start ---')
-    console.log(`  type: ${type}, to: ${id}`)
+    console.log(`  from: ${myPeerId.toString('hex').substring(0,6)}\n  to  : ${id.toString('hex').substring(0,6)}\n  type: ${type}`)
     console.log('--- writeMessageToPeers start ---')
     sendMessage(id, type, data)
   }
@@ -83,7 +108,7 @@ writeMessageToPeerToId = (toId, type, data) => {
   for (let id in peers) {
     if (id === toId) {
       console.log('--- writeMessageToPeerToId start ---')
-      console.log(`  type: ${type}, to: ${id}`)
+      console.log(`  from: ${myPeerId.toString('hex').substring(0,6)}\n  to  : ${id.toString('hex').substring(0,6)}\n  type: ${type}`)
       console.log('--- writeMessageToPeerToId start ---')
       sendMessage(id, type, data) 
     }
@@ -98,3 +123,7 @@ sendMessage = (id, type, data) => {
     data: data
   }))
 }
+
+setTimeout(() => {
+  writeMessageToPeers(MessageType.REQUEST_BLOCK, { index: chain.getLatestBlock().index + 1})
+}, 5000)
