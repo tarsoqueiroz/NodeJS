6d9d62c5cff34a4ab559952abfaca81ac1cc33bf
Tarso Queiroz <tarsoqueiroz@gmail.com>
2020-05-04 15:56:31 -0300

    Node.JS demo Static Shark site

---
+++ Template/StaticShark/app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app     = express();
+var router  = express.Router();
+
+var   path  = __dirname + '/views/';
+const PORT  = 8080;
+const HOST  = '0.0.0.0';
+
+router.use((req,res,next) => {
+  console.log("/" + req.method);
+  next();
+});
+
+router.get("/", (req,res) => {
+  res.sendFile(path + "index.html");
+});
+
+router.get("/sharks", (req,res) => {
+  res.sendFile(path + "sharks.html");
+});
+
+app.use(express.static(path));
+app.use("/", router);
+
+app.listen(8080, () => {
+  console.log('Example app listening on port 8080!')
+});
+
